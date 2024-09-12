4945ab441aa72c1058390ab97bb77d7ac763db11
thuy1901 <thuthuynguyen19012002@gmail.com>
2024-09-12 17:30:59 +0700

    update position locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,20 +1,22 @@
-// src/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { enLanguage, viLanguage } from './language';
+
+import enLanguage from '../public/locales/en.json';
+import viLanguage from '../public/locales/vi.json';
 
 i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        lng: 'en',
         fallbackLng: 'en',
         debug: true,
         interpolation: {
             escapeValue: false,
         },
         resources: {
-            en: enLanguage,
-            vi: viLanguage,
+            en: { translation: enLanguage },
+            vi: { translation: viLanguage },
         },
     });
 
--- src/language/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as viLanguage } from './viLanguage';
-export { default as enLanguage } from './enLanguage';
