ec02dbc0671f60c220d8a69d7b6302ccac4e2e53
thuy1901 <thuthuynguyen19012002@gmail.com>
2024-09-17 13:21:07 +0700

    update i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,16 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-import enLanguage from '../public/locales/en.json';
-import viLanguage from '../public/locales/vi.json';
+import HttpApi from 'i18next-http-backend';
 
 i18n.use(LanguageDetector)
+    .use(HttpApi)
     .use(initReactI18next)
     .init({
-        lng: 'en',
-        fallbackLng: 'en',
         debug: true,
-        interpolation: {
-            escapeValue: false,
-        },
-        resources: {
-            en: { translation: enLanguage },
-            vi: { translation: viLanguage },
+        lng: 'en',
+        backend: {
+            loadPath: '/locales/{{lng}}.json',
         },
     });
 
